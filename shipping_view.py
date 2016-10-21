641de505066309d54963c5581566108f1775d9b2
Trung Dung Do <thevigilante39@gmail.com>
2016-10-21 09:23:50 +0700

    Update shipping_view.py

--- shipping_view.py
+++ shipping_view.py
@@ -106,3 +106,90 @@ def shipping(request,shipping_number):
         #return render_to_response('shipping.html',RequestContext(request, context_dict))
     else:
         return  HttpResponseRedirect('/shops/')
+    
+def search_date(request):
+    year = request.GET['year']
+    month = request.GET['month']
+    user = Login.objects.get(user=request.user)
+    context_dict = {}
+    current_path = request.get_full_path()
+    context_dict['current_path'] = current_path
+    area_id = request.GET['area_id']
+    shop_id = request.GET['shop_id']
+    if area_id and shop_id:
+        area_name = Area.objects.get(pk=area_id)
+        shop_name = Shop.objects.get(pk=shop_id)
+        total_sale_list = Total.objects.filter(enable=1, year=year, month=month, shop=shop_id, customer_id=0)
+        i = len(total_sale_list)
+        sum = 0
+        new_total_sale_list = []
+        for total_sale in total_sale_list:
+            total_sale.shop_name = shop_name
+            total_sale.area_name = area_name
+            new_total_sale_list.append([total_sale, i])
+            sum = sum + total_sale.total
+            i -= 1
+        context_dict['area_name'] = area_name
+        context_dict['shop_name'] = shop_name
+        context_dict['area_select'] = int(area_id)
+        context_dict['shop_select'] = int(shop_id)
+    elif area_id and not shop_id:
+        area_name = Area.objects.get(pk=area_id)
+        shop_list = Shop.objects.filter(enable=1, area=area_id).distinct()
+        total_sale_list = Total.objects.filter(enable=1, year=year, month=month, shop__in=shop_list, customer_id=0)
+        i = len(total_sale_list)
+        sum = 0
+        new_total_sale_list = []
+        for total_sale in total_sale_list:
+            total_sale.shop_name = total_sale.shop_id
+            total_sale.area_name = area_name
+            new_total_sale_list.append([total_sale, i])
+            sum = sum + total_sale.total
+            i -= 1
+        context_dict['area_name'] = area_name
+        context_dict['area_select'] = int(area_id)
+        context_dict['shop_select'] = ''
+        if user.authority == 3 or user.authority == 9:
+            context_dict['shop'] = Shop.objects.filter(enable=1, area=area_id)
+        if user.authority == 9:
+            context_dict['area'] = Area.objects.filter(enable=1)
+    elif not area_id and shop_id:
+        area_list = Area.objects.filter(enable = 1)
+        shop_list = Shop.objects.filter(enable = 1)
+        shop = Shop.objects.filter(pk=int(shop_id))
+        total_sale_list = Total.objects.filter(enable=1, year=year, month=month, shop=shop, customer_id=0)
+        i = len(total_sale_list)
+        sum = 0
+        new_total_sale_list = []
+        for total_sale in total_sale_list:
+            total_sale.shop_name = Shop.objects.get(pk=total_sale.shop_id)
+            total_sale.area_name = Area.objects.get(pk=total_sale.shop_name.area_id)
+            new_total_sale_list.append([total_sale, i])
+            sum = sum + total_sale.total
+            i -= 1
+        context_dict['area_select'] = ''
+        context_dict['shop_select'] = ''
+        context_dict['area'] = area_list
+        context_dict['shop'] = shop_list
+    else:
+        area = Area.objects.filter(enable=1)
+        shop = Shop.objects.filter(enable=1)
+        total_sale_list = Total.objects.filter(enable=1, year=year, month=month, shop__in=shop, customer_id=0)
+        i = len(total_sale_list)
+        sum = 0
+        new_total_sale_list = []
+        for total_sale in total_sale_list:
+            total_sale.shop_name = Shop.objects.get(pk=total_sale.shop_id)
+            total_sale.area_name = Area.objects.get(pk=total_sale.shop_name.area_id)
+            new_total_sale_list.append([total_sale, i])
+            sum = sum + total_sale.total
+            i -= 1
+        context_dict['area_select'] = ''
+        context_dict['shop_select'] = ''
+        context_dict['area'] = area
+        context_dict['shop'] = shop
+    context_dict['total_sale_list'] = new_total_sale_list
+    context_dict['sum'] = sum
+    context_dict['year'] = year
+    context_dict['month'] = str(month).zfill(2)
+    return render_to_response('total-sale-list.html', RequestContext(request, context_dict))
